ebcfebe0313a2b47a960e24e50fb94fc37271f78
Lekhika <rlekhika.regmi@gmail.com>
2025-06-17 17:47:17 +0545

    Set up Containment Section

--- lib/categories/containment_widgets.dart
+++ lib/categories/containment_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_practice/theme/styles.dart';
 
 class ContainmentWidgetsPage extends StatefulWidget {
   const ContainmentWidgetsPage({super.key});
@@ -10,6 +11,58 @@ class ContainmentWidgetsPage extends StatefulWidget {
 class _ContainmentWidgetsPageState extends State<ContainmentWidgetsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('1.Containment', style: AppStyles.categoryTitle),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            AppStyles.categoryContentContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Alert Dialog', style: AppStyles.subcategoryTitle),
+                ],
+              ),
+            ),
+
+            AppStyles.categoryContentContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text('2.Card', style: AppStyles.subcategoryTitle)],
+              ),
+            ),
+
+            AppStyles.categoryContentContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('3.Divider', style: AppStyles.subcategoryTitle),
+                ],
+              ),
+            ),
+
+            AppStyles.categoryContentContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('4.List Tile', style: AppStyles.subcategoryTitle),
+                ],
+              ),
+            ),
+
+            AppStyles.categoryContentContainer(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('5.Bottom Sheet', style: AppStyles.subcategoryTitle),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
