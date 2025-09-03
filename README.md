d1bfdd164404d024504bb5a6fa149051c3a75082
Lekhika Regmi <rlekhika.regmi@gmail.com>
2025-09-03 16:07:40 +0545

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Flutter_Material_Widgets
 Beginner-friendly Flutter app to explore Material Design widgets with interactive examples and organized categories.
+
+---
+## 🎥 Demo
+
+Here’s how the app looks in action:
+
+<!-- Option 1: If you convert mp4 -> gif -->
+
+![Demo](assets/widgetapp.png)
+
+
+
+---
+
+
+
--- lib/categories/navigation_widgets.dart
+++ lib/categories/navigation_widgets.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../theme/styles.dart';
+import 'package:widget_practice/theme/styles.dart';
 
 class NavigationWidgetsPage extends StatefulWidget {
   const NavigationWidgetsPage({super.key});
@@ -14,6 +13,12 @@ class _NavigationWidgetsPageState extends State<NavigationWidgetsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Navigation', style: AppStyles.categoryTitle)),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[],
+        ),
+      ),
     );
   }
 }
